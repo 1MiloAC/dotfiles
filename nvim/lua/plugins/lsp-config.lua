8e5faa7ccd4e81591b7aa641bf58a206742f2fb1
miloa <miloa@miloas-MacBook-Air.local>
2025-07-20 17:25:06 -0500

    Switched away from lsp based formatting clients

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,33 +1,11 @@
 return {
     {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup({})
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                automatic_installation = true,
-                handlers = {
-                    ["lua_ls"] = function()
-                        require("lspconfig").lua_ls.setup({
-                            settings = {
-                                Lua = {
-                                    diagnostics = {
-                                        globals = { "vim" },
-                                    },
-                                },
-                            },
-                        })
-                    end,
-                    function(server_name)
-                        require("lspconfig")[server_name].setup({})
-                    end,
-                },
-            })
-        end,
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
     },
     {
         "neovim/nvim-lspconfig",
@@ -43,13 +21,6 @@ return {
         },
         config = function()
             local navbuddy = require("nvim-navbuddy")
-            --            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            --           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            --          vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-
-             require("lspconfig").wgsl_analyzer.setup({
-                 cmd = { "wgsl_analyzer" },
-             })
 
             vim.keymap.set("n", "<leader>b", function()
                 navbuddy.open()
